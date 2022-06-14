5ac3e7dc759009b031e3cfc146b28aaa1b32aff7
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-06-13 19:36:07 -0700

    Update script.js
    Hid text field of "other job role" upon loading of page

--- js/script.js
+++ js/script.js
@@ -3,29 +3,40 @@ const nameInput = `<input type="text" id="name"`
 
 //const nameInput = document.querySelector("name");
 
-//adding a focus element to highlight text fields when clicked
-let name = `<script type="text/javascript">
-function myFunction() {
-    document.getElementById("focus").focus();
+//adding a focus element to highlight text fields when clicked (this works)
+let name = 
+                `<script type="text/javascript">
+                function myFunction() {
+                    document.getElementById("focus").focus();
 }`
+//Hide the "text field" with the id of "other-job-role" so it is not displayed when the form first loads.(this works)
+
+const otherJob = document.getElementById('other-job-role').style.visibility = "hidden";
 
 
-const jobRole = `<label for="title">Job Role</label>
-<select id="title" name="user-title">`
+//Listening for a change event on Job Role (not sure if it's working)
+const jobRole = 
+                `<label for="title">Job Role</label>
+                <select id="title" name="other-job-role"> document.querySelector('Job Role')  
+                input.addEventListener('change', (e) => {  
+                console.log(e.target.value);  
+   });`
+
+//Make Other Job Role field appear if "other " is selected under Job Role
+
+//if (e.target.value === "Other") {
+   // display: "other job role";
+//}
+  
+
+
+
+
 
 
 
 
 //const jobRole = document.querySelector("Job Role");
-
-const otherJob = `<input type="text" name="other-job-role"`
-
-
-
-
-
-
-
 
 //console.log(otherJob)
 
