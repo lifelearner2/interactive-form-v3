9557c6e0bb53d52867c9051399e0d42052b3a82f
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-06-13 18:01:16 -0700

    Update script.js
    created variables, tested console with different ways of referencing elements

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 
+const nameInput = `<input type="text" id="name"`
 
+//const nameInput = document.querySelector("name");
 
-const nameInput = document.querySelector("name");
-
+//adding a focus element to highlight text fields when clicked
 let name = `<script type="text/javascript">
 function myFunction() {
     document.getElementById("focus").focus();
 }`
 
 
+const jobRole = `<label for="title">Job Role</label>
+<select id="title" name="user-title">`
+
+
+
+
+//const jobRole = document.querySelector("Job Role");
+
+const otherJob = `<input type="text" name="other-job-role"`
 
 
 
@@ -17,12 +27,10 @@ function myFunction() {
 
 
 
+//console.log(otherJob)
 
-
-
-
-
-
+//console.log(jobRole);
+//console.log(nameInput)
 //console.log(name);
 //console.log('test')
 
