fb28d44e9abddf6e88c43ba446eb9ff4836bcc92
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-06-13 17:33:20 -0700

    Update script.js
    Selected name and added focus to text fields

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-console.log('test')
+
+
+
+const nameInput = document.querySelector("name");
+
+let name = `<script type="text/javascript">
+function myFunction() {
+    document.getElementById("focus").focus();
+}`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//console.log(name);
+//console.log('test')
+
+
