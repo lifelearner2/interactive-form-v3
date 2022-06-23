5dde67d074c8ee2a048e7f84ef62bbd28c804257
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-06-23 11:14:59 -0700

    Update script.js
    Fixed errors in how I was selecting certain elements and using variables and added remaining cvv section code. Updated regex code.

--- js/script.js
+++ js/script.js
@@ -75,9 +75,9 @@ shirtDesigns.addEventListener("change", (e) => {
   }
 });
 
-//REGISTER FOR ACTIVITIES SECTION 
+//REGISTER FOR ACTIVITIES SECTION
 //Logging two variables - "fieldset(register for activities)" and "p (total)"
-const registerForActivities = document.querySelector("#activities");
+const registerForActivities = document.querySelector("#activities-box");
 const activitiesCost = document.querySelector(".activities-cost");
 
 console.log(registerForActivities);
@@ -88,10 +88,9 @@ let totalCost = 0;
 
 //Using the fieldset/register variable already created  to do the change/event listener
 registerForActivities.addEventListener("change", (e) => {
-  
   //inside e listener, creating variable to store "data cost" get.attribute of the e.target - getting cost of each item
   let dataCost = e.target.getAttribute("data-cost");
-  
+
   //turning dataCost from a string to a number with unary plus operator
   dataCost = +dataCost;
   console.log(dataCost);
@@ -99,11 +98,11 @@ registerForActivities.addEventListener("change", (e) => {
   //note: computer will not read numbers as a number when it's in a "string of quotes" so you need to use the unary plus sign operator to make it read as a number.
   //to test: log variable and log a second time with "typeOf" operator to ensure it's being read as a number.
   //You need to first select it in register section to print to console.
-  console.log(typeof(dataCost));
+  console.log(typeof dataCost);
 
   //inside e listener create if/else to listen if it was checked or unchecked. Use "check property"
   //if checked - adding the data cost of the selection to the total variable that was created earlier.
-  //if unchecked - then subtracting the data cost. 
+  //if unchecked - then subtracting the data cost.
   //Test by logging out total cost variable as well as checked property of e. target. You need to select activities to see it.
   if (e.target.checked) {
     totalCost += dataCost;
@@ -115,7 +114,7 @@ registerForActivities.addEventListener("change", (e) => {
   }
 
   //update the innerHTML of the total p element (p element name.innerHTML = template literal )
-  //example: activitiesTotalCostElement.textContent= `Total: $${activitiesTotalCost}`; 
+  //example: activitiesTotalCostElement.textContent= `Total: $${activitiesTotalCost}`;
   //This has two $$ because one is for the ${} and the other $ is for the literal dollar amount - the amount of the cost will be inside.
   activitiesCost.innerHTML = `Total: $${totalCost}`;
 });
@@ -123,7 +122,7 @@ registerForActivities.addEventListener("change", (e) => {
 //PAYMENT INFO SECTION:
 //created variables to reference paymentType, creditCard, paypal and bitcoin. Log to console.
 let paymentType = document.getElementById("payment");
-let creditCard = document.getElementById("credit-card");
+let creditCard = document.querySelector(".credit-card-box");
 let paypal = document.getElementById("paypal");
 let bitcoin = document.getElementById("bitcoin");
 
@@ -137,12 +136,12 @@ paypal = document.querySelector(".paypal").style.visibility = "hidden";
 bitcoin = document.querySelector(".bitcoin").style.visibility = "hidden";
 
 //used paymentType to target the second child and give it the selected property. Use .children property and setAttribute method.
-//I'm selecting the second child of paymentType with the number one. Zero is "select method", one gives credit card and two and three give the others. 
+//I'm selecting the second child of paymentType with the number one. Zero is "select method", one gives credit card and two and three give the others.
 //Selecting this (and using the word 'selected'(this equals credit card on html) makes credit card appear as a default when page is loaded.
 paymentType[1].setAttribute("selected", " ");
 
 //used paymentType variable to listen for a change event - when detected, displaying the div element w/id that matches the value of the e.target and hiding the other two div elements.
-//I created an event listener to listen for any changes on the paymentType. 
+//I created an event listener to listen for any changes on the paymentType.
 //I created a conditional statement if paypal is clicked to show the paypal info and hide other info and repeated this for bitcoin.
 //I also added another conditional statement for credit-card because if they clicked bitcoin or paypal and then wanted to go back to credit card I had to make sure the credit card info would display again and I did that with ' ' and then made the others hidden.
 paymentType.addEventListener("change", (e) => {
@@ -168,9 +167,9 @@ paymentType.addEventListener("change", (e) => {
 //FORM VALIDATION SECTION
 //used nameInput variable from earlier, created emailAddress variable, used registerForActivities variable from earlier, created cardNumber var, created zipCode var, create cvv and form variables. Log out.
 const emailAddress = document.getElementById("email");
-const cardNumber = document.getElementsByClassName("num-box");
-const zipCode = document.getElementsByClassName("zip-box");
-const cvv = document.getElementsByClassName("cvv-box");
+const cardNumber = document.querySelector("#cc-num");
+const zipCode = document.querySelector("#zip");
+const cvv = document.querySelector("#cvv");
 const form = document.querySelector("form");
 console.log(form);
 console.log(emailAddress);
@@ -195,8 +194,8 @@ function validationFail(element) {
 }
 console.log(validationFail);
 
-//created another var to store results of testing the name value var that was just created. 
-//Used regex to establish condition that the name must contain letters and accepts first and last name. 
+//created another var to store results of testing the name value var that was just created.
+//Used regex to establish condition that the name must contain letters and accepts first and last name.
 //This var will equal true if the test passes, false otherwise.
 const nameValidator = () => {
   const nameIsValid = /^[a-zA-Z]+ [a-zA-Z]+$/.test(nameInput.value);
@@ -228,12 +227,12 @@ const emailValidator = () => {
 
 //this section is testing if at least one box or activity was selected.
 const registerValidator = () => {
-  const registerSectionIsValid = registerForActivities.length > 0;
+  const registerSectionIsValid = totalCost > 0;
   if (registerSectionIsValid) {
     validationPass(registerForActivities);
+    let totalCost = registerSectionIsValid;
     console.log(
-      "Total number of activities registered: ",
-      `"${registerForActivities.value}"`
+      "The activities registered section evaluates to: ",`"${totalCost}"`
     );
   } else {
     validationFail(registerForActivities);
@@ -247,12 +246,12 @@ const registerValidator = () => {
 
 //this section ensures that the credit card number will be 13 to 16 digits.
 const creditCardValidator = () => {
-  const creditCardSectionIsValid = /^\d{13,16}/.test(creditCard.value);
+  const creditCardSectionIsValid = /^[0-9]{13,16}$/.test(cardNumber.value);
   if (creditCardSectionIsValid) {
-    validationPass(creditCard);
-    console.log("Credit Card entered is: ", `"${creditCard.value}"`);
+    validationPass(cardNumber);
+    console.log("Credit Card entered is: ", `"${cardNumber.value}"`);
   } else {
-    validationFail(creditCard);
+    validationFail(cardNumber);
     console.log(`This credit card validator prevented submission`);
   }
   console.log(
@@ -264,7 +263,7 @@ const creditCardValidator = () => {
 
 //this regex tests to ensure that the zip code entry is 5 digits.
 const zipCodeValidator = () => {
-  const zipCodeIsValid = /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(zipCode.value);
+  const zipCodeIsValid = /^[0-9]{5}$/.test(zipCode.value);
   if (zipCodeIsValid) {
     validationPass(zipCode);
     console.log("Zip code entered is: ", `"${zipCode.value}"`);
@@ -273,18 +272,23 @@ const zipCodeValidator = () => {
     console.log(`This zip code validator prevented submission`);
   }
   console.log(
-    `Zip code section validation test evaluates to ${zipCodeSectionIsValid}`
+    `Zip code section validation test evaluates to ${zipCode.value}`
   );
   return zipCodeIsValid;
 };
 
 //the cvv regex tests to see if there are 3 digits entered.
 const cvvValidator = () => {
-  const cvvIsValid = /^[0-9] {3}$/.test(cvv.value);
+  const cvvIsValid = /^[0-9]{3}$/.test(cvv.value);
   if (cvvIsValid) {
     validationPass(cvv);
     console.log("CVV entered is: ", `"${cvv.value}"`);
+  } else {
+    validationFail(cvv);
+    console.log(`This cvv validator prevented submission`);
   }
+  console.log(`cvv section validation test evaluates to ${cvv.value}`);
+  return cvvIsValid;
 };
 
 //using form var to listen for the submit event
@@ -296,12 +300,12 @@ form.addEventListener("submit", (e) => {
   if (!nameValidator()) {
     console.log("Invalid name prevented submission");
     e.preventDefault();
-  };
+  }
 
   if (!emailValidator()) {
     console.log("Invalid email prevented submission");
     e.preventDefault();
-  };
+  }
 
   if (!registerValidator()) {
     console.log("Invalid registration prevented submission");
@@ -311,23 +315,22 @@ form.addEventListener("submit", (e) => {
   if (!creditCardValidator()) {
     console.log("Invalid credit card prevented submission");
     e.preventDefault();
-  };
+  }
 
   if (!zipCodeValidator()) {
     console.log("Invalid zip code prevented submission");
     e.preventDefault();
-  };
+  }
 
   if (!cvvValidator()) {
     console.log("Invalid cvv prevented submission");
     e.preventDefault();
-  };
-  
+  }
+
   // try to create a helper functions for each required field that can be called in the e.listener to do the testing and return true or false depending on whether the field is valid or not.
 });
 
-
-//if you added an extra e.preventDefault() for testing, remove it. 
+//if you added an extra e.preventDefault() for testing, remove it.
 //at this point, the form should only be prevented from submitting if one or more of required form fields is invalid.
 //save and refresh. form should only submit and refresh if all required fields are valid.
 //be sure to  turn js back on before you submit project.Info on each payment method and all t shirt colors should also show when js if off.
@@ -336,18 +339,16 @@ form.addEventListener("submit", (e) => {
 
 //ACCESSIBILITY SECTION:
 //creating var to ref activities and logging it out.
-const activitiesCheckbox = document.getElementById('activities-box');
+const activitiesCheckbox = document.getElementById("activities-box");
 console.log(activitiesCheckbox);
 
 //used var above to loop over the activities' checkboxes.
 //added focus and blur events
-for (let i =0; i<activitiesCheckbox.length; i++) {
-  activitiesCheckbox[i].addEventListener ( 'focus', (e) => {
-    activitiesCheckbox[i].parentElement.classList.add('focus');
+for (let i = 0; i < activitiesCheckbox.length; i++) {
+  activitiesCheckbox[i].addEventListener("focus", (e) => {
+    activitiesCheckbox[i].parentElement.classList.add.focus;
   });
-  activitiesCheckbox[i].addEventListener('blur', (e) => {
-    activitiesCheckbox[i].parentElement.classList.remove('focus');
-  })
-};
-
-
+  activitiesCheckbox[i].addEventListener("blur", (e) => {
+    activitiesCheckbox[i].parentElement.classList.remove.focus;
+  });
+}
